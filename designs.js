2a2ccc672ea2e5e2b52f5e205295f072b37fadb4
Ozy Oforka <gentleozy2@gmail.com>
2018-06-30 10:50:48 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,45 +1,86 @@
+$(document).ready(function() {
+
+    $('#sizePicker').submit(function(event) {
+        clearGrid();
+// clearing a grid with while loop
+        makeGrid();
+        registerMouseHandlers();
+        event.preventDefault();
+        removeGrid();
+        clearColor();
+    });
+});
+
+//Create Grid using the attributes o the grid
 function makeGrid() {
-	console.log("makeGrid is running!")
-	
-	// Select size input
-	
-	var canvas, cell, gridHeight, gridWidth, rows;
-	
-	canvas = $('#pixel_canvas');
-	gridHeight = $('#input_height').val();
-	gridWidth = $('#input_width').val();
-	
-	canvas.children().remove()
-	
-	for (x = 0; x < gridHeight; x++) {
-	canvas.append('<tr></tr>');
-	}
-	
-	rows = $('tr');
-	
-	for (y = 0; y < gridWidth; y++) {
-	rows.append('<td></td>');
-	} 
-	
-	cell = canvas.find('td');
-	
-	// When td is clicked by the user, change color of td
-	cell.click(function() {
-		// Select color input
-		console.log("changeColor is running!");
-		var color;
-		color = $("#colorPicker").val();
-		$(this).attr('bgcolor', color);
-	});
-	
+    let inputH = $('#input_height').val();
+    let inputW = $('#input_width').val();
+    let table = $('#pixel_canvas');
+
+    for (let r = 1; r <= inputH; r++) {
+
+// To create Table Row
+   let row = $('<tr>      </tr>').appendTo(table);
+        
+     for (let c = 1; c <= inputW; c++) {
+// To Create Table Columns
+     row.append('<td></td>');
+        }
+    }
 }
 
-// When size is submitted by the user, call makeGrid()
-var submitQuery;
+// Clear Grid
+function clearGrid() {
+    $('#pixel_canvas').children().remove();
+}
 
-submitQuery = $('input[type="submit"]')
+// Color Multiple Grid by moving mouse over them //
 
-submitQuery.click(function(event) {
-  event.preventDefault();
-  makeGrid();
-});
+function registerMouseHandlers() {
+    
+    let mouseIsDown = false;
+
+    $('td').on('mousemove', function() {
+        if (mouseIsDown) {
+            let color = $('#colorPicker').val();
+            $(this).css('backgroundColor', color);
+        }
+    });
+
+    $('td').on('mousedown', function() {
+        mouseIsDown = true;
+    });
+
+    $('td').on('mouseup', function() {
+        mouseIsDown = false;
+    });
+
+    // adding color to single cell //
+    $('td').on('click', function() {
+        let color = $('#colorPicker').val();
+        $(this).css('backgroundColor', color);
+    });
+
+    // removing color from the cell //      
+    $('td').on('dblclick', function() {
+        $(this).css('background', 'none');
+    })
+}
+
+//To Remove all Grid
+const removeGrid = () => {
+  const removeGridButton = $('#removeGrid');
+  const grid = $('tr');
+  removeGridButton.click(() => {
+    grid.remove();
+  });
+}
+
+//To Clear Color
+const clearColor = () => {
+  const clearColorButton = $('#clearColor');
+  const gridCell = $('td');
+  clearColorButton.click(() => {
+    gridCell.removeAttr('style');
+  });
+}
